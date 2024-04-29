8d90f5ad110806d7e4f1d3dab176e2ce47e94dc3
GermanMunho <you@example.com>
2024-04-28 21:13:19 -0300

    ejercicio 2

--- TP4_GRUPO_2/src/ejercicio1/Ejercicio1.java
+++ TP4_GRUPO_2/src/ejercicio1/Ejercicio1.java
@@ -5,25 +5,121 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.CardLayout;
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Ejercicio1 extends JFrame {
 
 	
 	private static final long serialVersionUID = 1L;
+	Font fuente = new Font("Tahoma", Font.BOLD, 16);
 	private JPanel contentPane;
+	private JTextField textNombre;
+	private JTextField textApellido;
+	private JTextField textTelefono;
+	private JTextField textFecha;
+	
 
 	public Ejercicio1() {
 		setTitle("Ejercicio 1");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 572, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		 
+		contentPane.setLayout(null);
+		contentPane.setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(57, 58, 46, 14);
+		contentPane.add(lblNombre);
+		
+		JLabel lblApellido = new JLabel("Apellido:");
+		lblApellido.setBounds(57, 83, 52, 14);
+		contentPane.add(lblApellido);
+		
+		JLabel lblTelefono = new JLabel("Telefono:");
+		lblTelefono.setBounds(57, 108, 46, 26);
+		contentPane.add(lblTelefono);
+		
+		JLabel lblFecha = new JLabel("Fecha Nacimiento:");
+		lblFecha.setBounds(57, 145, 99, 14);
+		contentPane.add(lblFecha);
+		
+		textNombre = new JTextField();
+		textNombre.setBounds(174, 55, 86, 20);
+		contentPane.add(textNombre);
+		textNombre.setColumns(10);
+		
+		textApellido = new JTextField();
+		textApellido.setBounds(174, 80, 86, 20);
+		contentPane.add(textApellido);
+		textApellido.setColumns(10);
+		
+		textTelefono = new JTextField();
+		textTelefono.setBounds(174, 111, 86, 20);
+		contentPane.add(textTelefono);
+		textTelefono.setColumns(10);
+		
+		textFecha = new JTextField();
+		textFecha.setBounds(174, 142, 86, 20);
+		contentPane.add(textFecha);
+		textFecha.setColumns(10);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.setBounds(127, 186, 89, 23);
+		contentPane.add(btnAceptar);
+		
+		JLabel labelMostrar = new JLabel("");
+		labelMostrar.setBounds(20, 220, 500, 20);
+		contentPane.add(labelMostrar);
+		btnAceptar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				 String nombre = textNombre.getText();
+	             String apellido = textApellido.getText();
+	             String telefono = textTelefono.getText();
+	             String fechaNacimiento = textFecha.getText();
+	             String datos= null;
+	                textNombre.setBackground(nombre.trim().isEmpty()? Color.RED:UIManager.getColor("TextField.background") );
+	                textApellido.setBackground(apellido.trim().isEmpty()? Color.RED: UIManager.getColor("TextField.background"));
+	                textTelefono.setBackground(telefono.trim().isEmpty()? Color.RED: UIManager.getColor("TextField.background"));
+	                textFecha.setBackground(fechaNacimiento.trim().isEmpty()? Color.RED: UIManager.getColor("TextField.background"));
+	            
+	                if((nombre.trim().isEmpty() || apellido.trim().isEmpty() || telefono.trim().isEmpty() || fechaNacimiento.trim().isEmpty())) {
+	                    datos = "Los datos ingresados fueron: ";
+	                }
+	                else {
+	                    datos = "Los datos ingresados fueron: " + nombre + " " + apellido + ", " + telefono + ", " + fechaNacimiento;
+	                } 
+	                
+	                labelMostrar.setText(datos);
+	                
+	                textNombre.setText("");
+	                textApellido.setText("");
+	                textTelefono.setText("");
+	                textFecha.setText("");
+			}
+		});
 	}
-
-
 }
